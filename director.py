cc6434bea0d8734ff0b9249d1be3ca7d86e9bc44
slowedpepper7 <97586351+slowedpepper7@users.noreply.github.com>
2022-01-21 14:57:43 -0700

    Update director.py

--- director.py
+++ director.py
@@ -11,6 +11,7 @@ class Director:
 
     def user_input(self):
         card_guess = input("Will the next card be higher or lower? H/L")
+        self.is_playing = (card_guess == "H") or (card_guess == "L"
        
     def update(self):
         pass
